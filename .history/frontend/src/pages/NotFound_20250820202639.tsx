1060e6a2f23ad1c6792e7612f78505fccc77a3a3
Shreyasaraf01 <shreyasaraf765@gmail.com>
2025-09-03 01:28:19 +0530

    Ignore .history folder

--- .history/frontend/src/pages/NotFound_20250820202639.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="p-3 text-center">
-      <h1 className="display-5 fw-bold">404</h1>
-      <p className="text-secondary">Page not found</p>
-    </div>
-  );
-}
